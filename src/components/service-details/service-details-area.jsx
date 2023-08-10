b943bc906c4eae264eb336d3919dc32089024940
sabithmohamad <sabithmohamad1@gmail.com>
2023-08-10 20:13:16 +0530

    first commit

--- src/components/service-details/service-details-area.jsx
+++ src/components/service-details/service-details-area.jsx
@@ -9,8 +9,8 @@ const ServiceDetailsArea = () => {
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details">
                 <h3 className="tp-title-sm service-details-space">Expand Your Reach: Connecting Universities with Global Talent</h3>
-                {/* <a href="#">Schedule a free expert session <i className="fal fa-arrow-right"></i></a> */}
-              </div>
+{/* <a href="#">Schedule a free expert session &gt;<i className="fal fa-arrow-right"></i></a> */}
+</div>
             </div>
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details-paragraph">
@@ -26,7 +26,7 @@ const ServiceDetailsArea = () => {
           <div className="row">
             <div className="col-12">
               <div className="sd-big-img">
-                <img src="/assets/img/service/service-icon-10.1.jpg" alt="" />
+              <Image src="/assets/img/service/service-icon-10.1.jpg" alt="" />
               </div>
             </div>
             <div className="col-xl-6 col-lg-6">
@@ -37,10 +37,11 @@ const ServiceDetailsArea = () => {
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details-paragraph">
                 <p className="pb-15">In the ever-evolving landscape of education, LGR takes the lead in connecting universities with top-tier talent from across the globe. Our innovative approach streamlines the process for institutions seeking the best students, saving valuable time by connecting them exclusively with high-quality, pre-vetted applicants.</p>
-                <p>With "Biggest College Fairs and Webinars," LGR offers a dynamic platform for universities to extend their global presence. Engage with prospective students through highly acclaimed events, fostering a worldwide reputation and expanding reach through extensive promotional channels.
+                <p>With Biggest College Fairs and Webinars, LGR offers a dynamic platform for universities to extend their global presence. Engage with prospective students through highly acclaimed events, fostering a worldwide reputation and expanding reach through extensive promotional channels.
 
-Navigating the vast terrain of international recruitment, LGR's "1000+ Recruitment Partner Network" stands as a testament to our dedication. Drawing from years of experience, we've cultivated a robust network of over 1000+ active recruitment partners. This expansive community collaborates diligently to deliver a diverse and talented student pool to your institution, ensuring excellence and enriching the academic journey for both students and universities alike..
-                </p>
+Navigating the vast terrain of international recruitment, LGR's &quot;1000+ Recruitment Partner Network&quot; stands as a testament to our dedication. Drawing from years of experience, we&apos;ve cultivated a robust network of over 1000+ active recruitment partners. This expansive community collaborates diligently to deliver a diverse and talented student pool to your institution, ensuring excellence and enriching the academic journey for both students and universities alike.
+</p>
+
               </div>
             </div>
           </div>

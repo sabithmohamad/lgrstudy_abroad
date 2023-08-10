3a59be42d60edbefe1484915963d6ab5f4580a4b
sabithmohamad <sabithmohamad1@gmail.com>
2023-08-10 21:31:22 +0530

    first commit

--- src/components/service-details/service-details-area.jsx
+++ src/components/service-details/service-details-area.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
+import Image from 'next/image'; // Import the Image component from next/image
 
 const ServiceDetailsArea = () => {
   return (
@@ -10,31 +10,26 @@ const ServiceDetailsArea = () => {
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details">
                 <h3 className="tp-title-sm service-details-space">Expand Your Reach: Connecting Universities with Global Talent</h3>
-{/* <a href="#">Schedule a free expert session &gt;<i className="fal fa-arrow-right"></i></a> */}
-</div>
+                {/* <a href="#">Schedule a free expert session &gt;<i className="fal fa-arrow-right"></i></a> */}
+              </div>
             </div>
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details-paragraph">
-                <p className="pb-15">Embark on a journey of enhanced global outreach and engagement with LGR. Leverage our years of expertise and an expansive network of 1000+ recruitment partners to propel your university's presence to unprecedented heights.</p>
-                <p className="pb-10"> By partnering with LGR, your institution gains the advantage of receiving meticulously vetted applications from high-quality students, backed by our reputation for excellence. Additionally, access the largest college fairs and immersive webinars, enriching your university's exposure and connections. Collaborate with over 1000+ recruitment partners to foster a worldwide community, ensuring your university's continued growth and prominence on the global education stage.</p>
-                {/* <p>A dedicated product design team can help you achieve your business goals. Whether you need to
-                  craft an idea for a completely new product or elevate the quality of an existing solution, we’ll
-                  help you to create a product that is laser targeted to your users’ needs and delivers business
-                  results</p> */}
+                <p className="pb-15">Embark on a journey of enhanced global outreach and engagement with LGR. Leverage our years of expertise and an expansive network of 1000+ recruitment partners to propel your university&apos;s presence to unprecedented heights.</p>
+                <p className="pb-10">By partnering with LGR, your institution gains the advantage of receiving meticulously vetted applications from high-quality students, backed by our reputation for excellence. Additionally, access the largest college fairs and immersive webinars, enriching your university&apos;s exposure and connections. Collaborate with over 1000+ recruitment partners to foster a worldwide community, ensuring your university&apos;s continued growth and prominence on the global education stage.</p>
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col-12">
-            <div className="sd-big-img">
-  <Image
-    src="/assets/img/service/service-icon-10.1.jpg"
-    alt=""
-    width={1500}
-    height={800}
-  />
-</div>
-
+              <div className="sd-big-img">
+                <Image
+                  src="/assets/img/service/service-icon-10.1.jpg"
+                  alt=""
+                  width={1500}
+                  height={800}
+                />
+              </div>
             </div>
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details">
@@ -44,11 +39,8 @@ const ServiceDetailsArea = () => {
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details-paragraph">
                 <p className="pb-15">In the ever-evolving landscape of education, LGR takes the lead in connecting universities with top-tier talent from across the globe. Our innovative approach streamlines the process for institutions seeking the best students, saving valuable time by connecting them exclusively with high-quality, pre-vetted applicants.</p>
-                <p>With Biggest College Fairs and Webinars, LGR offers a dynamic platform for universities to extend their global presence. Engage with prospective students through highly acclaimed events, fostering a worldwide reputation and expanding reach through extensive promotional channels.
-
-Navigating the vast terrain of international recruitment, LGR's &quot;1000+ Recruitment Partner Network&quot; stands as a testament to our dedication. Drawing from years of experience, we&apos;ve cultivated a robust network of over 1000+ active recruitment partners. This expansive community collaborates diligently to deliver a diverse and talented student pool to your institution, ensuring excellence and enriching the academic journey for both students and universities alike.
-</p>
-
+                <p>With Biggest College Fairs and Webinars, LGR offers a dynamic platform for universities to extend their global presence. Engage with prospective students through highly acclaimed events, fostering a worldwide reputation and expanding reach through extensive promotional channels.</p>
+                <p>Navigating the vast terrain of international recruitment, LGR&apos;s &quot;1000+ Recruitment Partner Network&quot; stands as a testament to our dedication. Drawing from years of experience, we&apos;ve cultivated a robust network of over 1000+ active recruitment partners. This expansive community collaborates diligently to deliver a diverse and talented student pool to your institution, ensuring excellence and enriching the academic journey for both students and universities alike.</p>
               </div>
             </div>
           </div>
--- src/components/services/service-1/service-area.jsx
+++ src/components/services/service-1/service-area.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
 
-
 const ServiceDetailsArea = () => {
   return (
     <>
@@ -10,16 +9,13 @@ const ServiceDetailsArea = () => {
           <div className="row service-pt-pb">
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details">
-                <h3 className="tp-title-sm service-details-space">How LGR can assist Recruitment Partners
-</h3>
+                <h3 className="tp-title-sm service-details-space">How LGR can assist Recruitment Partners</h3>
                 {/* <a href="#">Schedule a free expert session <i className="fal fa-arrow-right"></i></a> */}
               </div>
             </div>
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details-paragraph">
-              <p className="pb-15">Connect with over 100 colleges with our unique course finding engine to elevate your journey, experience rich commissions, and access a world of opportunity.
-
-.</p>
+                <p className="pb-15">Connect with over 100 colleges with our unique course finding engine to elevate your journey, experience rich commissions, and access a world of opportunity.</p>
                 <p className="pb-10">Experience the advantage of our rich commission structure that rewards your efforts as you guide students towards their dreams. As a dedicated partner of LGR, you'll enjoy unparalleled benefits that extend beyond just monetary gains. We believe in fostering strong relationships and providing the necessary resources to help you excel in your role as an educational consultant.</p>
                 {/* <p>A dedicated product design team can help you achieve your business goals. Whether you need to
                   craft an idea for a completely new product or elevate the quality of an existing solution, we’ll
@@ -31,12 +27,12 @@ const ServiceDetailsArea = () => {
           <div className="row">
             <div className="col-12">
               <div className="sd-big-img">
-              <Image
-    src="/assets/img/service/service-icon-10.1.jpg"
-    alt=""
-    width={1500}
-    height={800}
-  />
+                <Image
+                  src="/assets/img/service/service-icon-10.1.jpg"
+                  alt=""
+                  width={1500}
+                  height={800}
+                />
               </div>
             </div>
             <div className="col-xl-6 col-lg-6">
@@ -47,8 +43,7 @@ const ServiceDetailsArea = () => {
             <div className="col-xl-6 col-lg-6">
               <div className="sd-service-details-paragraph">
                 <p className="pb-15">At LGR Study Abroad, we stand as your unwavering partner, ensuring an admission process you can rely on, dedicated customer support, and the industry's most rewarding commissions. With access to a vast network of 100+ esteemed universities, we are your gateway to a world of educational opportunities, elevating your consultancy journey to new heights.</p>
-                <p>At LGR Study Abroad, we're not just a consultancy, we're your partners in shaping academic futures and turning dreams into reality.
-                </p>
+                <p>At LGR Study Abroad, we're not just a consultancy, we're your partners in shaping academic futures and turning dreams into reality.</p>
               </div>
             </div>
           </div>
